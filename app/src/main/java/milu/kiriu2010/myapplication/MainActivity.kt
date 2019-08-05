94a068f3faa4790ba94871eb621f48084edd0c2f
milukiriu2010 <milu.kiriu2010@gmail.com>
2019-08-05 20:38:58 +0900

    path effect

--- app/src/main/java/milu/kiriu2010/myapplication/MainActivity.kt
+++ app/src/main/java/milu/kiriu2010/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package milu.kiriu2010.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import milu.kiriu2010.myapplication.a0x.a01.A0XActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // A0xページへ遷移
+        btnA0X.transformationMethod = null
+        btnA0X.setOnClickListener {
+            val intent = Intent(this, A0XActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
